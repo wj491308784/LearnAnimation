1ab3c5e7b6d5097933c0f1f803ed449c16a8d388
wj491308784 <491308784@qq.com>
2018-11-15 11:58:18 +0800

    Initial Commit

---
+++ ButtonAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ButtonAnimation
+//
+//  Created by WJia on 2018/11/15.
+//  Copyright © 2018年 WJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ButtonAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ButtonAnimation
+//
+//  Created by WJia on 2018/11/15.
+//  Copyright © 2018年 WJia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
